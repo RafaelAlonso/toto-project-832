f497a16852a3eeae0c38a64f96a06a48516fbdf4
Rafael Alonso <rafael.palonso@gmail.com>
2022-03-09 10:23:18 -0300

    dependencies

--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
 from setuptools import setup
+from setuptools import find_packages
+
+# list dependencies from file
+with open('requirements.txt') as f:
+    content = f.readlines()
+requirements = [x.strip() for x in content]
 
 setup(name='toto',
-      description="pacote teste para o batch #832",
-      packages=["toto"])
+      description="package description",
+      packages=find_packages(),
+      install_requires=requirements)
--- toto/hello.py
+++ toto/hello.py
@@ -1,2 +1,4 @@
+from termcolor import colored
+
 def say_hello():
-  print('Hello batch #832! It is a lovely day! Does it work?')
+  print(colored('Hello batch #832! It is a lovely day! Does it work?', 'green'))
